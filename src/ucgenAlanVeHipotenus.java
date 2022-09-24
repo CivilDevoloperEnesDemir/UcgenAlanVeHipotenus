7f114509e1b5e0e42cc254b381e35f915518dbd0
CivilDevoloperEnesDemir <114176807+CivilDevoloperEnesDemir@users.noreply.github.com>
2022-09-24 15:06:23 +0300

    Update ucgenAlanVeHipotenus.java

--- src/ucgenAlanVeHipotenus.java
+++ src/ucgenAlanVeHipotenus.java
@@ -1,5 +1,27 @@
-public class Main {
+import java.util.Scanner;
+public class HypotenuseCalculateProgram {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inp = new Scanner(System.in);
+
+        double FirstEdge , SecondEdge , Hypotenuse;
+
+        System.out.print("Please Enter First Edge Length of Triangle: ");
+        FirstEdge = inp.nextDouble();
+        System.out.print("Please Enter Second Edge of Triangle : ");
+        SecondEdge = inp.nextDouble();
+
+        Double FESquare = Math.pow(FirstEdge,2);
+        Double SESquare = Math.pow(SecondEdge,2);
+        Hypotenuse = FESquare+SESquare;
+        Hypotenuse = Math.pow(Hypotenuse,0.5);
+
+
+
+        System.out.print("The Length of Hypotenuse is : " + Hypotenuse);
+
+
+
+
+
     }
 }
